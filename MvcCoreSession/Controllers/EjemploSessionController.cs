4718f07516477f0239301db7b06129fa2d521bba
Carlos00-pixel <carlos.mayoral00@gmail.com>
2023-03-01 10:24:33 +0100

    Add project files.

---
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        int numero = 1;
+
+        public IActionResult Index()
+        {
+            ViewData["NUMERO"] = "Valor del numero: " + this.numero;
+            return View();
+        }
+
+        public IActionResult SessionSimple(string accion)
+        {
+            if(accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    HttpContext.Session.SetString("nombre", "Programeitor");
+                    HttpContext.Session.SetString("hora", DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "Datos almacenados en Session";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    ViewData["USUARIO"] = HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] = HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
+    }
+}
